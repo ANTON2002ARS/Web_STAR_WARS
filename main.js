7b163b9afb6ddfb260595d5e91724f116fdf8c38
__ANTON_2002__ <101361041+ANTON2002ARS@users.noreply.github.com>
2024-07-05 18:16:12 +0300

    codex

---
+++ main.js
@@ -0,0 +1,51 @@
+let obg_films = document.getElementById("films_lines");
+let obj_Story = document.getElementById("storyline");
+let obj_Codex = document.getElementById("codex");
+let obj_Technic = document.getElementById("technic");
+
+//времено
+All_Close();
+
+function Show_Films() {
+  All_Close();
+  if (obg_films.style.display == "none") {
+    obg_films.style.display = "grid";
+    obj_Story.style.display = "none";
+  } else {
+    obg_films.style.display = "none";
+  }
+}
+
+function Show_Story_Line() {
+  All_Close();
+  if (obj_Story.style.display == "none") {
+    obj_Story.style.display = "block";
+  } else {
+    obj_Story.style.display = "none";
+  }
+}
+
+function Show_Heros() {
+  All_Close();
+  if (obj_Codex.style.display == "none") {
+    obj_Codex.style.display = "flex";
+  } else {
+    obj_Codex.style.display = "none";
+  }
+}
+
+function Show_Technic() {
+  All_Close();
+  if (obj_Technic.style.display == "none") {
+    obj_Technic.style.display = "flex";
+  } else {
+    obj_Technic.style.display = "none";
+  }
+}
+
+function All_Close() {
+  obg_films.style.display = "none";
+  obj_Story.style.display = "none";
+  obj_Codex.style.display = "none";
+  obj_Technic.style.display = "none";
+}
